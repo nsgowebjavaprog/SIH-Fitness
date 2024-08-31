d313c686679ace4f0bc958ad9ecbe87aa31ff891
NS258 <154657596+nsgowebjavaprog@users.noreply.github.com>
2024-08-31 12:47:19 +0530

    Add files via upload
    added

---
+++ Navbar.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React from "react";
+import logo from "@/assets/logo.png";
+import { IoIosBody } from "react-icons/io";
+import "./Navbar.css";
+import Image from "next/image";
+import Link from "next/link";
+import AuthPopup from "../AuthPopup/AuthPopup";
+const Navbar = () => {
+  const [isloggedin, setIsloggedin] = React.useState<boolean>(false);
+
+  const [showpopup, setShowpopup] = React.useState<boolean>(false);
+  // const checklogin = async () => {
+  //   fetch(process.env.NEXT_PUBLIC_BACKEND_API + "/auth/checklogin", {
+  //     method: "POST",
+  //     credentials: "include",
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       if (data.ok) {
+  //         setIsloggedin(true);
+  //       } else {
+  //         setIsloggedin(false);
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
+
+  // React.useEffect(() => {
+  //   checklogin();
+  // }, [showpopup]);
+
+  return (
+    <nav>
+      <Image src={logo} alt="Logo" />
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/profile">
+        <IoIosBody />
+      </Link>
+
+      {isloggedin ? (
+        <button>Logout</button>
+      ) : (
+        <button
+          onClick={() => {
+            setShowpopup(true);
+          }}
+        >
+          Login
+        </button>
+      )}
+
+      {/* {isloggedin ? (
+        <button>Logout</button>
+      ) : (
+        <button
+          onClick={() => {
+            setShowpopup(true);
+          }}
+        >
+          Login
+        </button>
+      )} */}
+
+      {/* {showpopup && <AuthPopup setShowpopup={setShowpopup} />} */}
+
+      {showpopup && <AuthPopup setShowpopup={setShowpopup}/>}
+    </nav>
+  );
+};
+
+export default Navbar;
